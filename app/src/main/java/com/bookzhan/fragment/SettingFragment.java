6766de0dc92e16e112f7b4ab554cb2783642f0b8
zhandalin <zhandalin@readtv.cn>
2016-04-05 22:29:14 +0800

    gg
    fff

--- app/src/main/java/com/bookzhan/fragment/SettingFragment.java
+++ app/src/main/java/com/bookzhan/fragment/SettingFragment.java
@@ -1,22 +1,46 @@
 package com.bookzhan.fragment;
 
+<<<<<<< HEAD
+=======
+import android.app.Activity;
+>>>>>>> parent of 9cd9c5c... 登陆界面调整完成
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.bookzhan.activity.SettingMenuNameActivity;
 import com.bookzhan.activity.SettingVideoActivity;
 import com.bookzhan.video.R;
 import com.zhanlibrary.base.BaseFragment;
+import com.zhanlibrary.base.Constants;
+import com.zhanlibrary.utils.GHSStringUtil;
+import com.zhanlibrary.utils.SpUtils;
 
 /**
  * Created by zhandalin on 2016-04-03 13:50.
  * 说明:
  */
 public class SettingFragment extends BaseFragment implements View.OnClickListener {
+<<<<<<< HEAD
+=======
+
+    private final int MAIN_VIDEO_RESULT_CODE = 10;
+    private final int LEFT_VIDEO_RESULT_CODE = 11;
+    private final int BOTTOM_VIDEO_RESULT_CODE = 12;
+    private final int RIGHT_VIDEO_RESULT_CODE = 13;
+    private final int TOP_VIDEO_RESULT_CODE = 14;
+
+
+    private TextView tv_main_video;
+    private TextView tv_left_video;
+    private TextView tv_bottom_video;
+    private TextView tv_right_video;
+    private TextView tv_top_video;
+>>>>>>> parent of 9cd9c5c... 登陆界面调整完成
 
     @Nullable
     @Override
@@ -27,14 +51,54 @@ public class SettingFragment extends BaseFragment implements View.OnClickListene
     }
 
     private void initView(View view) {
+<<<<<<< HEAD
         view.findViewById(R.id.tv_setting_menu_name).setOnClickListener(this);
         view.findViewById(R.id.tv_setting_video_file).setOnClickListener(this);
         view.findViewById(R.id.tv_setting_picture).setOnClickListener(this);
         view.findViewById(R.id.tv_setting_music).setOnClickListener(this);
+=======
+        tv_main_video = (TextView) view.findViewById(R.id.tv_main_video);
+        view.findViewById(R.id.ll_choose_main_video).setOnClickListener(this);
+        tv_left_video = (TextView) view.findViewById(R.id.tv_left_video);
+        view.findViewById(R.id.ll_choose_left_video).setOnClickListener(this);
+        tv_bottom_video = (TextView) view.findViewById(R.id.tv_bottom_video);
+        view.findViewById(R.id.ll_choose_bottom_video).setOnClickListener(this);
+        tv_right_video = (TextView) view.findViewById(R.id.tv_right_video);
+        view.findViewById(R.id.ll_choose_right_video).setOnClickListener(this);
+        tv_top_video = (TextView) view.findViewById(R.id.tv_top_video);
+        view.findViewById(R.id.ll_choose_top_video).setOnClickListener(this);
+
+        String main_video_path = (String) SpUtils.get(context, Constants.VideoPath.MAIN_VIDEO_KEY, "");
+        String left_video_path = (String) SpUtils.get(context, Constants.VideoPath.LEFT_VIDEO_KEY, "");
+        String bottom_video_path = (String) SpUtils.get(context, Constants.VideoPath.BOTTOM_VIDEO_KEY, "");
+        String right_video_path = (String) SpUtils.get(context, Constants.VideoPath.RIGHT_VIDEO_KEY, "");
+        String top_video_path = (String) SpUtils.get(context, Constants.VideoPath.TOP_VIDEO_KEY, "");
+        if (!GHSStringUtil.isEmpty(main_video_path)) {
+            tv_main_video.setText(main_video_path);
+            tv_main_video.setVisibility(View.VISIBLE);
+        }
+        if (!GHSStringUtil.isEmpty(left_video_path)) {
+            tv_left_video.setText(left_video_path);
+            tv_left_video.setVisibility(View.VISIBLE);
+        }
+        if (!GHSStringUtil.isEmpty(bottom_video_path)) {
+            tv_bottom_video.setText(bottom_video_path);
+            tv_bottom_video.setVisibility(View.VISIBLE);
+        }
+        if (!GHSStringUtil.isEmpty(right_video_path)) {
+            tv_right_video.setText(right_video_path);
+            tv_right_video.setVisibility(View.VISIBLE);
+        }
+        if (!GHSStringUtil.isEmpty(top_video_path)) {
+            tv_top_video.setText(top_video_path);
+            tv_top_video.setVisibility(View.VISIBLE);
+        }
+>>>>>>> parent of 9cd9c5c... 登陆界面调整完成
     }
 
     @Override
     public void onClick(View v) {
+<<<<<<< HEAD
         Intent intent;
         switch (v.getId()) {
             case R.id.tv_setting_menu_name:
@@ -55,4 +119,68 @@ public class SettingFragment extends BaseFragment implements View.OnClickListene
                 break;
         }
     }
+=======
+        switch (v.getId()) {
+            case R.id.ll_choose_main_video:
+                openFileExplorer(MAIN_VIDEO_RESULT_CODE);
+                break;
+            case R.id.ll_choose_left_video:
+                openFileExplorer(LEFT_VIDEO_RESULT_CODE);
+                break;
+            case R.id.ll_choose_bottom_video:
+                openFileExplorer(BOTTOM_VIDEO_RESULT_CODE);
+                break;
+            case R.id.ll_choose_right_video:
+                openFileExplorer(RIGHT_VIDEO_RESULT_CODE);
+                break;
+            case R.id.ll_choose_top_video:
+                openFileExplorer(TOP_VIDEO_RESULT_CODE);
+                break;
+        }
+    }
+
+    public void openFileExplorer(int requestCode) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.setType("video/*.mp4");
+        startActivityForResult(intent, requestCode);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            String uri = data.getData().getPath();
+            switch (requestCode) {
+                case MAIN_VIDEO_RESULT_CODE:
+                    tv_main_video.setText(uri);
+                    tv_main_video.setVisibility(View.VISIBLE);
+                    SpUtils.put(context, Constants.VideoPath.MAIN_VIDEO_KEY, uri);
+                    break;
+                case LEFT_VIDEO_RESULT_CODE:
+                    tv_left_video.setText(uri);
+                    tv_left_video.setVisibility(View.VISIBLE);
+                    SpUtils.put(context, Constants.VideoPath.LEFT_VIDEO_KEY, uri);
+                    break;
+                case BOTTOM_VIDEO_RESULT_CODE:
+                    tv_bottom_video.setText(uri);
+                    tv_bottom_video.setVisibility(View.VISIBLE);
+                    SpUtils.put(context, Constants.VideoPath.BOTTOM_VIDEO_KEY, uri);
+                    break;
+                case RIGHT_VIDEO_RESULT_CODE:
+                    SpUtils.put(context, Constants.VideoPath.RIGHT_VIDEO_KEY, uri);
+                    tv_right_video.setVisibility(View.VISIBLE);
+                    tv_right_video.setText(uri);
+                    break;
+                case TOP_VIDEO_RESULT_CODE:
+                    SpUtils.put(context, Constants.VideoPath.TOP_VIDEO_KEY, uri);
+                    tv_top_video.setVisibility(View.VISIBLE);
+                    tv_top_video.setText(uri);
+                    break;
+            }
+        }
+
+    }
+>>>>>>> parent of 9cd9c5c... 登陆界面调整完成
 }
